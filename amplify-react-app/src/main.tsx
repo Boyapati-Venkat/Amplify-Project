d7f9dd0ae6254143e689724a4d017c71f60a56f6
BoyapatiVenkat <venkatboyapaati@gmail.com>
2025-05-29 03:47:56 +0530

    new

--- amplify-react-app/src/main.tsx
+++ amplify-react-app/src/main.tsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports.ts';
+import awsExports from './aws-exports.js';
 
 // Configure Amplify
 Amplify.configure(awsExports);
